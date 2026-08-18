793a63a61e7d88cb646869f808bdf60b45321080
Patrik Wenger <paddor@gmail.com>
2026-08-18 22:38:21 +0200

    Fix JSR publish license normalization
    wasm-pack appends a blank line to jsr/LICENSE during the build. Normalize the generated change so deno publish sees a clean working tree on main.

--- jsr/build.sh
+++ jsr/build.sh
@@ -13,6 +13,11 @@ cd wasm
 wasm-pack build --target web --release --out-dir "../$TMP"
 cd ..
 
+# wasm-pack appends a blank line to the package license.
+awk 'NF { last = NR } { lines[NR] = $0 } END { for (i = 1; i <= last; i++) print lines[i] }' \
+  LICENSE > LICENSE.tmp
+mv LICENSE.tmp LICENSE
+
 cp "$TMP/burli_wasm.js" "$PKG/"
 cp "$TMP/burli_wasm.d.ts" "$PKG/"
 cp "$TMP/burli_wasm_bg.wasm.d.ts" "$PKG/"
